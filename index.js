f04c335949fc62b2d3e8791f95ff2c62537681a5
Johnny Gonzales <jsg420@gmail.com>
2017-12-29 17:37:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ function logShout(string) {
   return console.log().toUpperCase()
 }
 
-function logShout(string){
+function logShout(string) {
   return console.log(string.toUpperCase())
 }
 
-function logWhisper(string){
+function logWhisper(string) {
  return console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
+function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) return "I can't hear you!";
   else if (string === "I love you, Grandma.") return "I love you, too.";
   else if (string === string.toUpperCase()) return "YES INDEED!"
